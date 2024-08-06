bdce9baefa346df60c46cb331cec661134462c9a
greta novak <114183568+elfarr@users.noreply.github.com>
2024-08-06 19:36:07 +0300

    add 2 board for images

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 
-from telegram import Message, Update
-from telegram.ext import CommandHandler, MessageHandler, filters, ApplicationBuilder, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Message, Update
+from telegram.ext import CommandHandler, MessageHandler, filters, ApplicationBuilder, ContextTypes, CallbackQueryHandler
 import requests, re
-from io import BytesIO
 from config import *
 from constants import sticker_colors
 import random
@@ -13,18 +12,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     
 WAITING_FOR_TEXT = "waiting_for_text"
+WAITING_FOR_PHOTO = "waiting_for_photo"
+
 async def start_comment_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data[WAITING_FOR_TEXT] = True
-    await update.message.reply_text('Отправьте текст в формате <сообщение> -c <цвет стикера>]')
+    await update.message.reply_text('Отправьте текст в формате <сообщение> -c <цвет стикера>')
 
 
-    
 async def comment_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if context.user_data.get(WAITING_FOR_TEXT):
+    if context.user_data.get(WAITING_FOR_TEXT) and update.message.text[0] != '/':
         pattern = re.compile(r'^(.*?)\s*-c\s*(\S+)$')
         match = pattern.match(update.message.text)
         if match:
-            
             text = match.group(1).strip()
             color = match.group(2).strip()
             if color not in sticker_colors: color = 'light_yellow'
@@ -34,7 +33,7 @@ async def comment_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.message.from_user
         comment_text = f"Комментарий от {user.first_name} {user.last_name}: {text}"
         
-        url = f"https://api.miro.com/v2/boards/{MIRO_BOARD_ID}/sticky_notes"
+        url = f"https://api.miro.com/v2/boards/{BOARDS[0]["id"]}/sticky_notes"
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
@@ -64,51 +63,65 @@ async def comment_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         
 async def color_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = "Доступные цвета:\n"
-    print(sticker_colors)
     for color in sticker_colors:
         message += f"- {color}\n"
     await update.message.reply_text(message)
 
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = "Действие было отменено"
+    context.user_data[WAITING_FOR_TEXT] = False
+    context.user_data[WAITING_FOR_PHOTO] = False
+    await update.message.reply_text(message)
 
-WAITING_FOR_PHOTO = "waiting_for_photo"
+
 async def start_photo_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Обработчик команды /photo, который запрашивает фото."""
+    keyboard = [[InlineKeyboardButton(board["name"], callback_data=board["id"])] for board in BOARDS]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Выберите доску для загрузки фото:', reply_markup=reply_markup)
+
+async def select_board(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    board_id = query.data
+    print(query)
+    print(board_id)
+    context.user_data["selected_board_id"] = board_id
     context.user_data[WAITING_FOR_PHOTO] = True
-    await update.message.reply_text('Пожалуйста, отправьте фото, чтобы я мог загрузить его на Miro.')
+    await query.answer()
+    await query.edit_message_text('Доска выбрана. Пожалуйста, отправьте фото.')
 
 
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.user_data.get(WAITING_FOR_PHOTO):
         try:
-            message: Message = update.message
-            print(f"Получено сообщение: {message}")
-
-            # Проверка наличия фото
+            message = update.message
             if message.photo:
                 photo = message.photo[-1]
                 photo_file = await photo.get_file()
                 file_url = photo_file.file_path
-         
-                miro_url = f"https://api.miro.com/v2/boards/{MIRO_BOARD_ID}/images"
+                selected_board_id = context.user_data.get("selected_board_id")
+
+                if not selected_board_id:
+                    await message.reply_text('Не выбрана доска. Пожалуйста, начните сначала.')
+                    return
+
+                miro_url = f"https://api.miro.com/v2/boards/{selected_board_id}/images"
                 headers = {
                     "accept": "application/json",
                     "authorization": f"Bearer {MIRO_API_TOKEN}"
                 }
 
                 data = {
-            "data": {
-                "title": "Sample image title",
-                "url": file_url
-            },
-            "position": {
-                 "x": 2000+random.random()*1000,
-                "y": 2000+random.random()*1000
-            }
-        }
+                    "data": {
+                        "title": "Sample image title",
+                        "url": file_url
+                    },
+                    "position": {
+                        "x": 2000 + random.random() * 1000,
+                        "y": 2000 + random.random() * 1000
+                    }
+                }
                 
                 response = requests.post(miro_url, headers=headers, json=data)
-                print(f"Ответ от Miro API: {response.status_code}")
-
                 if response.status_code == 201:
                     await message.reply_text('Фотография успешно добавлена на доску Miro.')
                 else:
@@ -119,25 +132,30 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await message.reply_text('Пожалуйста, отправьте фото.')
 
         except Exception as e:
-            print(f"Произошла ошибка: {e}")
-            await message.reply_text('Произошла ошибка при обработке фотографии. Пожалуйста, попробуйте еще раз.')
-
+            await message.reply_text(f'Произошла ошибка: {e}')
 
 def main():
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
+    cancel_handler = CommandHandler('cancel', cancel)
+
     comment_handler = CommandHandler('comment', start_comment_command)
     application.add_handler(start_handler)
+    application.add_handler(cancel_handler)
     application.add_handler( comment_handler)
+
+
 
     photo_command_handler = CommandHandler("photo", start_photo_command)
     application.add_handler(photo_command_handler)
-
-    color_hangler = CommandHandler("color", color_command)
+    application.add_handler(CallbackQueryHandler(select_board))
     photo_handler = MessageHandler(filters.PHOTO, handle_photo)
     handler_comment = MessageHandler(filters.TEXT, comment_command)
+    
+    color_hangler = CommandHandler("color", color_command)
     application.add_handler(color_hangler)
+
     application.add_handler(photo_handler)
     application.add_handler(handler_comment)
     application.run_polling()
